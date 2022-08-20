8c10f3a894d1783473fdcc506b3c36ef7a9c8757
Ronald kubasu <ronald.kubasu@student.moringaschool.com>
2022-08-20 13:12:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name,event){
+    let cards = [];
+    for (let i=0;i<name.length;i++){
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+}
+return cards
+    return cards
+}
+
+function countDown(num){
+    for (let i=num;i>=0;i--){
+        console.log(i)
+    }
+} 
